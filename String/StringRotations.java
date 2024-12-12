ddf3269c2200426e4f58a3b843dc7792e19a084f
Your Name <your.email@example.com>
2024-12-13 00:50:17 +0530

    String Completed

---
+++ String/StringRotations.java
@@ -0,0 +1,9 @@
+public static boolean areRotations(String s1, String s2) {
+        // Your code here
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        
+        String concat = s1 + s1;
+        return concat.lastIndexOf(s2) >= 0;
+    }
